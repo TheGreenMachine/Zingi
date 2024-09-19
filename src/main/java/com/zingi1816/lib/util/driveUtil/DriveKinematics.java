b2a8b33d320ccfbd638485c3c5e31694ecaeb9ea
ishwaqo8120 <ishwaqo26358@isd273.org>
2024-09-19 18:04:58 -0500

    Reset of the Repository

--- src/main/java/com/zingi1816/lib/util/driveUtil/DriveKinematics.java
+++
@@ -1,8 +0,0 @@
-package com.zingi1816.lib.util.driveUtil;
-
-import com.team1816.lib.util.team254.DriveSignal;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-
-public interface DriveKinematics {
-    DriveSignal inverseKinematics(ChassisSpeeds chassisSpeeds);
-}
